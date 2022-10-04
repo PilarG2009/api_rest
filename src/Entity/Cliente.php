8535ca7c3c9ac010ea40ba7b29cf5f5ea0bf3e25
pilar <pilar.gomis@solvam.es>
2022-10-04 16:22:56 +0200

    Proyecto con entidades y relaciones

---
+++ src/Entity/Cliente.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Cliente
+ *
+ * @ORM\Table(name="cliente")
+ * @ORM\Entity
+ */
+class Cliente
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255, nullable=false)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellidos", type="string", length=255, nullable=false)
+     */
+    private $apellidos;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="telefono", type="integer", nullable=false)
+     */
+    private $telefono;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Direccion::class, mappedBy="cliente", orphanRemoval=true)
+     */
+    private $direcciones;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Pedido::class, mappedBy="cliente")
+     */
+    private $pedidos;
+
+    public function __construct()
+    {
+        $this->direcciones = new ArrayCollection();
+        $this->pedidos = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellidos(): ?string
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos(string $apellidos): self
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?int
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono(int $telefono): self
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Direccion>
+     */
+    public function getDirecciones(): Collection
+    {
+        return $this->direcciones;
+    }
+
+    public function addDireccione(Direccion $direccione): self
+    {
+        if (!$this->direcciones->contains($direccione)) {
+            $this->direcciones[] = $direccione;
+            $direccione->setCliente($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDireccione(Direccion $direccione): self
+    {
+        if ($this->direcciones->removeElement($direccione)) {
+            // set the owning side to null (unless already changed)
+            if ($direccione->getCliente() === $this) {
+                $direccione->setCliente(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Pedido>
+     */
+    public function getPedidos(): Collection
+    {
+        return $this->pedidos;
+    }
+
+    public function addPedido(Pedido $pedido): self
+    {
+        if (!$this->pedidos->contains($pedido)) {
+            $this->pedidos[] = $pedido;
+            $pedido->setCliente($this);
+        }
+
+        return $this;
+    }
+
+    public function removePedido(Pedido $pedido): self
+    {
+        if ($this->pedidos->removeElement($pedido)) {
+            // set the owning side to null (unless already changed)
+            if ($pedido->getCliente() === $this) {
+                $pedido->setCliente(null);
+            }
+        }
+
+        return $this;
+    }
+
+
+}
